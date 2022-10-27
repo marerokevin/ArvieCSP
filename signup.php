64c6768d4d8ec490ced600c28532265d70365575
marerokevin <marerokevin@gmail.com>
2022-10-27 13:20:56 +0800

    [Refactor] Codes and registration

--- signup.php
+++ signup.php
@@ -47,12 +47,7 @@
                 </div>
                 <div class="relative mb-6 hidden">
                     <label for="first_name" class="block mb-2 text-sm font-medium text-gray-900">Sponsor</label>
-<<<<<<< HEAD
                     <input type="text" id="name-input" name="sponsor" class="bg-gray-50 border border-gray-300 text-gray-900 text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
-=======
-                    <input type="text" id="sponsorId-input" name="sponsor" class="bg-gray-50 border border-gray-300 text-gray-900 text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
-                    <button type="button" class="checkID text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2">Check Referral Code</button>
->>>>>>> abcbc3e511b4b37a45011dbb219228fc16275eef
                 </div>
                 <div class="grid gap-6 mb-6 md:grid-cols-2">
                     <div>
