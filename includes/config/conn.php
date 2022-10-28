adb1664c800826c2deb80886fdb1376da8fa8a50
marerokevin <marerokevin@gmail.com>
2022-10-28 10:21:53 +0800

    [Database] Connection with my database

--- includes/config/conn.php
+++ includes/config/conn.php
@@ -1,14 +1,10 @@
 <?php
     $server = "localhost";
-    $username = "ArvieCSP";
+    $username = "arviecsp";
     $password = "p@55w0rd";
     $database = "ArvieDS";
 
     if(!$conn = mysqli_connect($server, $username, $password, $database)){
         die("Failed to Connect to Database!");
     }
-
-    // if(!$conn = mysqli_connect($server, $username, $password, $database)){
-    //     die("Failed to Connect to Database!");
-    // } else $conn = mysqli_connect($server, $username, $password, $database);
 ?>
